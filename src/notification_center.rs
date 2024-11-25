2f9cd2c2c3164db898c9c15004d74c770b7b90fd
Modeckrus <modeckrus.official.page@gmail.com>
2024-11-25 16:46:20 +0300

    some fixes

--- src/notification_center.rs
+++ src/notification_center.rs
@@ -1,11 +1,11 @@
+use leptos::{IntoView, RwSignal, SignalUpdate};
 use std::sync::Arc;
-use leptos::{RwSignal, IntoView, SignalUpdate};
 
-use leptos::*;
 use leptos::logging::*;
+use leptos::*;
 
 #[component]
-pub fn NotificationCenterBuilder() -> impl IntoView{
+pub fn NotificationCenterBuilder() -> impl IntoView {
     view! {
         <div class="bottom-0 top-auto fixed right-0 left-auto gap-2">
             <InternalNotificationCenter/>
@@ -14,7 +14,7 @@ pub fn NotificationCenterBuilder() -> impl IntoView{
 }
 
 #[component]
-fn InternalNotificationCenter() -> impl IntoView{
+fn InternalNotificationCenter() -> impl IntoView {
     let center = NotificationCenter::new();
     provide_context(center.clone());
     view! {
@@ -30,7 +30,7 @@ fn InternalNotificationCenter() -> impl IntoView{
 }
 
 #[component]
-pub fn Notification(notification: Notification) -> impl IntoView{
+pub fn Notification(notification: Notification) -> impl IntoView {
     let status = notification.status;
     let data = notification.data;
     view! {
@@ -66,80 +66,79 @@ pub fn Notification(notification: Notification) -> impl IntoView{
     }
 }
 
-
 #[derive(Clone, Copy, PartialEq, Eq)]
-pub struct NotificationCenter{
-    pub notifications: RwSignal<Vec<Notification>>
+pub struct NotificationCenter {
+    pub notifications: RwSignal<Vec<Notification>>,
 }
 
-impl NotificationCenter{
-    pub fn new() -> Self{
-        Self{
-            notifications: RwSignal::new(vec![])
+impl NotificationCenter {
+    pub fn new() -> Self {
+        Self {
+            notifications: RwSignal::new(vec![]),
         }
     }
 
-    pub fn error(&self, s: impl Into<String>){
+    pub fn error(&self, s: impl Into<String>) {
         let notification = Notification::error(s);
         let id = notification.id;
         self.notifications.update(|n| n.push(notification));
         let s = self.clone();
         leptos::spawn_local(async move {
-            crate::c::sleep(std::time::Duration::from_secs(50)).await;
+            crate::c::sleep(std::time::Duration::from_secs(5)).await;
             s.remove(id);
         })
     }
 
-    pub fn info(&self, s: impl Into<String>){
+    pub fn info(&self, s: impl Into<String>) {
         self.notifications.update(|n| n.push(Notification::info(s)));
     }
 
-    pub fn view(&self, view: impl IntoView){
-        self.notifications.update(|n| n.push(Notification::view(view)));
+    pub fn view(&self, view: impl IntoView) {
+        self.notifications
+            .update(|n| n.push(Notification::view(view)));
     }
 
-    pub fn remove(&self, id: i32){
+    pub fn remove(&self, id: i32) {
         self.notifications.update(|n| n.retain(|n| n.id != id));
     }
 }
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct Notification{
+pub struct Notification {
     pub id: i32,
     pub data: NotificationData,
     pub status: RwSignal<NotificationStatus>,
 }
 
-
-impl Notification{
-    pub fn new(data: NotificationData) -> Self{
-        Self{
+impl Notification {
+    pub fn new(data: NotificationData) -> Self {
+        Self {
             id: rand::random(),
             data,
-            status: RwSignal::new(NotificationStatus::Appearing)
+            status: RwSignal::new(NotificationStatus::Appearing),
         }
     }
 
-    pub fn error(s: impl Into<String>) -> Self{
+    pub fn error(s: impl Into<String>) -> Self {
         Notification::new(NotificationData::Error(s.into()))
     }
 
-    pub fn info(s: impl Into<String>) -> Self{
+    pub fn info(s: impl Into<String>) -> Self {
         Notification::new(NotificationData::Info(s.into()))
     }
 
-    pub fn view(view: impl IntoView) -> Self{
+    pub fn view(view: impl IntoView) -> Self {
         Notification::new(NotificationData::View(Arc::new(view.into_view())))
     }
 }
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum NotificationStatus{
+pub enum NotificationStatus {
     Appearing,
     Idle,
     Disappearing,
 }
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub enum NotificationData{
+pub enum NotificationData {
     Error(String),
     Info(String),
-    View(Arc<leptos::View>)
+    View(Arc<leptos::View>),
 }
